8af2a3ec12f7c6bd4e59d7eb938fbb68e1428c53
Lindsay Spencer <lindsay.spencer@logistyx.com>
2021-10-12 09:36:37 -0500

    26. Assignment 4 - Hotel Amenities.

---
+++ HiddenVilla/HiddenVilla_Server/Model/Hotel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace HiddenVilla_Server.Model
+{
+    public class Hotel
+    {
+        public List<BlazorRoom> Rooms { get; set; }
+        public List<BlazorAmenity> Amenities { get; set; }
+    }
+}
