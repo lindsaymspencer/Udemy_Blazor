35c817083d245f4717f838116c3fd69ec7f033d6
Lindsay Spencer <lindsay.spencer@logistyx.com>
2021-10-22 15:49:59 -0500

    72. JSRuntime Extensions

---
+++ HiddenVilla/HiddenVilla_Server/Helpers/IJSRuntimeExtension.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using Microsoft.JSInterop;
+
+namespace HiddenVilla_Server.Helpers
+{
+    public static class IJSRuntimeExtension
+    {
+        public static async ValueTask ToasterSuccess(this IJSRuntime JSRuntime, string message)
+        {
+            await JSRuntime.InvokeVoidAsync("ShowToastr", "success", message);
+        }
+
+        public static async ValueTask ToasterError(this IJSRuntime JSRuntime, string message)
+        {
+            await JSRuntime.InvokeVoidAsync("ShowToastr", "error", message);
+        }
+    }
+}
