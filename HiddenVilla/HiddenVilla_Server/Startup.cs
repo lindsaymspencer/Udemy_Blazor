d3aedc20bcef8b27ec45327df50e5e6587ffb06b
Lindsay Spencer <lindsay.spencer@logistyx.com>
2021-10-22 14:58:21 -0500

    67. Create First Hotel Room.

--- HiddenVilla/HiddenVilla_Server/Startup.cs
+++ HiddenVilla/HiddenVilla_Server/Startup.cs
@@ -1,4 +1,6 @@
 using System;
+using Business.Repository;
+using Business.Repository.IRepository;
 using DataAccess.Data;
 using HiddenVilla_Server.Data;
 using Microsoft.AspNetCore.Builder;
@@ -25,6 +27,7 @@ namespace HiddenVilla_Server
         {
             services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+            services.AddScoped<IHotelRoomRepository, HotelRoomRepository>();
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
