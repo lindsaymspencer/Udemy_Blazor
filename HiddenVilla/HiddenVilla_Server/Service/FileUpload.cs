93851ec1ebbd22a0115850ca425da3efafbea373
Lindsay Spencer <lindsay.spencer@logistyx.com>
2021-10-27 10:07:25 -0500

    83. Implement File Upload Interface

--- HiddenVilla/HiddenVilla_Server/Service/FileUpload.cs
+++ HiddenVilla/HiddenVilla_Server/Service/FileUpload.cs
@@ -1,22 +1,71 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.IO;
 using System.Threading.Tasks;
 using HiddenVilla_Server.Service.IService;
 using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Hosting;
 
 namespace HiddenVilla_Server.Service
 {
     public class FileUpload :IFileUpload
     {
-        public Task<string> UploadFile(IBrowserFile file)
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public FileUpload(IWebHostEnvironment webHostEnvironment)
         {
-            throw new NotImplementedException();
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        public async Task<string> UploadFile(IBrowserFile file)
+        {
+            try
+            {
+                FileInfo fileInfo = new FileInfo(file.Name);
+                var fileName = Guid.NewGuid().ToString() + fileInfo.Extension;
+                var folderDirectory = $"{_webHostEnvironment.WebRootPath}\\RoomImages";
+                var path = Path.Combine(_webHostEnvironment.WebRootPath, "RoomImages", fileName);
+
+                var memoryStream = new MemoryStream();
+                await file.OpenReadStream().CopyToAsync(memoryStream);
+
+                if(!Directory.Exists(folderDirectory))
+                {
+                    Directory.CreateDirectory(folderDirectory);
+                }
+
+                await using var fs = new FileStream(path, FileMode.Create, FileAccess.Write);
+                memoryStream.WriteTo(fs);
+
+                var fullPath = $"RoomImages/{fileName}";
+
+                return fullPath;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
         }
 
         public bool DeleteFIle(string fileName)
         {
-            throw new NotImplementedException();
+            bool status = false;
+            try
+            {
+                var path = Path.Combine(_webHostEnvironment.WebRootPath, "RoomImages", fileName);
+                if (File.Exists(path))
+                {
+                    File.Delete(path);
+                    return true;
+                }
+
+                return false;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
         }
     }
 }
