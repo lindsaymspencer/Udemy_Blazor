93d32523c8f23f1ceed1bbf8e9ecbe802a36f010
Lindsay Spencer <lindsay.spencer@logistyx.com>
2021-10-27 09:53:57 -0500

    82. File Upload Interface

---
+++ HiddenVilla/HiddenVilla_Server/Service/FileUpload.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HiddenVilla_Server.Service.IService;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace HiddenVilla_Server.Service
+{
+    public class FileUpload :IFileUpload
+    {
+        public Task<string> UploadFile(IBrowserFile file)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteFIle(string fileName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
